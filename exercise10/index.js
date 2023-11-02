decab377cdd9a5a335719be93e568d41ec531b11
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-03 00:22:20 +0500

    Add files via upload

---
+++ exercise10/index.js
@@ -0,0 +1,10 @@
+// 10:Adding Comments: Choose two of the programs you’ve written, and add at least
+//  one comment to each. If you don’t have anything specific to write because your 
+// programs are too simple at this point, just add your name and the current date 
+// at the top of each program file. Then write one sentence describing what the program does.
+// addition program
+var c = 20;
+var d = 18;
+console.log("".concat(c, "  +  ").concat(d, " = ").concat(c + d));
+//subtraction program
+console.log("".concat(c, " - ").concat(d, "  = ").concat(c - d));
---
+++ exercise10/index.ts
@@ -0,0 +1,14 @@
+// 10:Adding Comments: Choose two of the programs you’ve written, and add at least
+//  one comment to each. If you don’t have anything specific to write because your 
+// programs are too simple at this point, just add your name and the current date 
+// at the top of each program file. Then write one sentence describing what the program does.
+
+// addition program
+
+let c = 20;
+let d = 18;
+console.log(`${c}  +  ${d} = ${c+d}`);
+
+//subtraction program
+
+console.log(`${c} - ${d}  = ${c-d}`)
