5522a04324d348254cfb9e00485ca95184f9cf7f
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-03 00:18:11 +0500

    Add files via upload

---
+++ exercise 7/index.js
@@ -0,0 +1,9 @@
+// 7:Number Eight: Write addition, subtraction, multiplication, and division operations 
+// that each result in the number 8. Be sure to enclose your operations in print statements 
+// to see the results.
+var a = 10;
+var b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
