a964a7882a076a90ea71c2ab3ec0aaeb8b556916
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-03 00:08:28 +0500

    Add files via upload

---
+++ exercise-4/index.ts
@@ -0,0 +1,5 @@
+//4:Famous Quote: Find a quote from a famous person you admire.
+//  Print the quote and the name of its author. Your output should look something
+//  like the following, including the quotation marks:
+
+console.log(`Imam Ali said,"half of your beauty comes from the the way you speak"`);
