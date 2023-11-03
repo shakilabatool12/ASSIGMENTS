ef65c0951d4b96509997967e7df390b928af2e1c
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:28:36 +0500

    Add files via upload

---
+++ exercise 26/index.js
@@ -0,0 +1,16 @@
+//26:
+var alien_colour = 'green';
+if (alien_colour === 'green') {
+    console.log("congratulation ! you just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Congratulation! you just earned 10 points for shooting the alien.");
+}
+console.log("\n second version of code");
+alien_colour = 'blue';
+if (alien_colour === 'green') {
+    console.log("Congratulation! you just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Congratulations! you just earned 10 points to shooting the alien.");
+}
---
+++ exercise 26/index.ts
@@ -0,0 +1,21 @@
+//26:
+
+
+let alien_colour:string = 'green';
+
+if(alien_colour === 'green'){
+    console.log(`congratulation ! you just earned 5 points for shooting the alien.`);
+}else{
+    console.log(`Congratulation! you just earned 10 points for shooting the alien.`);
+}
+
+console.log(`\n second version of code`)
+alien_colour ='blue';
+if(alien_colour === 'green')
+{
+    console.log(`Congratulation! you just earned 5 points for shooting the alien.`);
+
+}else{
+    console.log(`Congratulations! you just earned 10 points to shooting the alien.`)
+}
+
