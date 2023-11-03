461921297fe240eb93d8e35064805142dae2989c
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:21:43 +0500

    Add files via upload

---
+++ exercise 19/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 19:Dinner Guests: Working with one of the programs from Exercises 14 through
+//  18, print a message indicating the number of people you are inviting to din
+Object.defineProperty(exports, "__esModule", { value: true });
+//Importing guestlist from assig15
+var assig14_1 = require("../assig14");
+//console.log(`\n printing message indicating number of guest`)
+console.log("We had finally invited ".concat(assig14_1.guestList.length, " guest in assig15"));
---
+++ exercise 19/index.ts
@@ -0,0 +1,11 @@
+
+// 19:Dinner Guests: Working with one of the programs from Exercises 14 through
+//  18, print a message indicating the number of people you are inviting to din
+
+//Importing guestlist from assig15
+
+import{guestList} from "../assig14"
+//console.log(`\n printing message indicating number of guest`)
+console.log(`We had finally invited ${guestList.length} guest in assig15`)
+
+
