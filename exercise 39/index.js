f0dd997229288deab9ce5ab45c32ca2b60c826ca
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:45:35 +0500

    Add files via upload

---
+++ exercise 39/index.js
@@ -0,0 +1,7 @@
+//39: 
+var city_country = function (city, country) {
+    return "".concat(city, ", ").concat(country);
+};
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("new york", "USA"));
+console.log(city_country("Istanbol", "Turkey"));
