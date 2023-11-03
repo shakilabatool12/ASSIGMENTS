f433cfd5735c3839efbad1baa0b62fad02264062
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:36:42 +0500

    Add files via upload

---
+++ exercise 31/index.ts
@@ -0,0 +1,18 @@
+
+//3: Name Cases: Store a person’s name in a variable, and then print that person’s name 
+// in lowercase, uppercase, and titlecase.
+
+let firstName1= "zamal fatima";
+console.log(firstName1);
+
+//lower Case
+
+console.log(firstName1.toLowerCase());
+
+//Upper Case
+
+console.log(firstName1.toUpperCase());
+
+//Title Case
+
+console.log(firstName1.charAt(0).toUpperCase()+firstName1.slice(1));
