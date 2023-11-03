c984f46af0e0f4b97add30856f29dbb0833fa52f
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:29:46 +0500

    Add files via upload

---
+++ exercise 27/index.ts
@@ -0,0 +1,49 @@
+//exercise 27:
+
+//version 1st
+
+console.log(`1st version where alien colour is Green.`)
+
+let alien_colour:string = 'green';
+
+if(alien_colour === 'green'){
+    console.log(`congratulation ! you just earned 5 points.`);
+}else if(alien_colour === 'yellow'){
+    console.log(`Congratulation! you just earned 10 points.`)
+}else if(alien_colour === 'red'){
+    console.log(`Congratulation! you just earned 15 points.`)
+}else {
+    console.log(`unknown alien colour.`)
+}
+
+//second version
+
+console.log(`\n Second version where alien colour is Yellow.`)
+
+ alien_colour = 'yellow';
+
+if(alien_colour === 'green'){
+    console.log(`congratulation ! you just earned 5 points.`);
+}else if(alien_colour === 'yellow'){
+    console.log(`Congratulation! you just earned 10 points.`)
+}else if(alien_colour === 'red'){
+    console.log(`Congratulation! you just earned 15 points.`)
+}else {
+    console.log(`unknown alien colour.`)
+}
+
+//version 3rd
+
+console.log(`\n Third version where alien colour is Red.`)
+
+ alien_colour = 'red';
+
+if(alien_colour === 'green'){
+    console.log(`congratulation ! you just earned 5 points.`);
+}else if(alien_colour === 'yellow'){
+    console.log(`Congratulation! you just earned 10 points.`)
+}else if(alien_colour === 'red'){
+    console.log(`Congratulation! you just earned 15 points.`)
+}else {
+    console.log(`unknown alien colour.`)
+}
