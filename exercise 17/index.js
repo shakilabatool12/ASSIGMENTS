35fa084a4d9d4317658ecf13ef87e2966a7c8380
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:19:06 +0500

    Add files via upload

---
+++ exercise 17/index.js
@@ -0,0 +1,24 @@
+// 17:Shrinking Guest List: You just found out that your new dinner
+//  table won’t arrive in time for the dinner, and you have space for only two guests.
+// • Start with your program from Exercise 16. Add a new line that prints a
+//  message saying that you can invite only two people for dinner.
+//• Remove guests from your list one at a time until only two names
+//remain in your list. Each time you pop a name from your list, print
+//a message to that person letting them know you’re sorry you can’t invite them to dinner.
+// • Print a message to each of the two people still on your list, letting 
+// them know they’re still invited.
+// • Remove the last two names from your list, so you have an empty list. 
+// Print your list to make sure you actually have an empty list at the end of your program.
+var guestlist = ["zamal", "yashab", "sara", "fatima"];
+console.log("I can only invite only two people");
+while (guestlist.length > 2) {
+    var removeGuest = guestlist.pop();
+    console.log("sorry,".concat(removeGuest, ",I cant invite you to dinner"));
+}
+for (var _i = 0, guestlist_1 = guestlist; _i < guestlist_1.length; _i++) {
+    var guest = guestlist_1[_i];
+    console.log("Hello,".concat(guest, ", you are still invited to dinner!"));
+}
+guestlist.pop();
+guestlist.pop();
+console.log("Guest list after dinner ", guestlist);
