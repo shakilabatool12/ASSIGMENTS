915e96bfb08301ffc9b95dbb1ee1f22272ab7632
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:27:33 +0500

    Add files via upload

---
+++ exercise 25/index.ts
@@ -0,0 +1,13 @@
+let alien_color = 'green'
+
+if(alien_color == 'green')
+{
+    console.log("congratulations! you just earned 5 points.")
+}
+
+alien_color = 'blue'
+
+if(alien_color == 'green')
+{
+    console.log("congratulations! you just earned 5 points.")
+}
