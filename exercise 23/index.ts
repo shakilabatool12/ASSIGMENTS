c93bd6385918f01011e919b5e023f065c65060a6
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:25:47 +0500

    Add files via upload

---
+++ exercise 23/index.ts
@@ -0,0 +1,57 @@
+//Conditional Tests: Write a series of conditional tests.
+//  Print a statement describing each test and your prediction
+//  for the results of each test. Your code should look something like this:
+
+//let car = 'subaru';
+
+//console.log("Is car == 'subaru'? I predict True.")
+
+//console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand
+//  why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True
+//  and another 5 tests evaluate to False.
+
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru')//true
+
+console.log("Is car == 'honda'? I predict false.");
+console.log(car == 'honda')//false
+
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru')//true
+
+console.log("Is car == 'Subaru'? I predict false.");
+console.log(car == 'Subaru')//false
+
+console.log("Is car !== 'toyota'? I predict True.");
+console.log(car !== 'toyota')//true
+
+console.log("Is car !== 'subaru'? I predict false.");
+console.log(car !== 'subaru')//false
+
+console.log("Is car !== 'mazda'? I predict True.");
+console.log(car !== 'mazda')//true
+
+console.log("Is car != 'subaru'? I predict false.");
+console.log(car != 'subaru')//false
+
+let age =25;
+console.log("Is age >18 ?I pridect true.");
+console.log(age>18);
+
+console.log("Is age <22 ?I pridect false.");
+console.log(age<22);
+
+
+
+
+
+
+
+
