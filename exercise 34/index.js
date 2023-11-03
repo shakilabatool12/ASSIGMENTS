36434fabd84239d3ed49458535bd55c149450f7a
BINT E AYOUB <145936626+shakilabatool12@users.noreply.github.com>
2023-11-04 00:40:11 +0500

    Add files via upload

---
+++ exercise 34/index.js
@@ -0,0 +1,18 @@
+//34:Pizzas: Think of at least three kinds of your favorite pizza. 
+//Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+//• Modify your for loop to print a sentence using the name of the pizza
+// instead of printing just the name of the pizza. For each pizza you should have
+// one line of output containing a simple statement like I like pepperoni pizza.
+//• Add a line at the end of your program, outside the for loop, that states how
+// much you like pizza. The output should consist of three or more lines about
+// the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+var favoritepizzas = ['pepperoni', 'margherita', 'BBQ chicken'];
+console.log("My favorite pizzas are:");
+for (var i = 0; i < favoritepizzas.length; i++) {
+    console.log(favoritepizzas[i]);
+}
+console.log("\nI like these pizzas:");
+for (var i = 0; i < favoritepizzas.length; i++) {
+    console.log("I like" + favoritepizzas[i] + "Pizza.");
+}
+console.log("\nI really love pizza!");
